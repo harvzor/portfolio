061863c096c59c12118e7b679db2893318153341
Harvey <harvey@growcreate.co.uk>
2015-04-22 23:31:51 +0100

    Further styling. Added Disqus and started post on post.ejs.

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,10 @@
+var body = document.getElementsByTagName('body')[0];
 var toggles = document.getElementsByClassName('toggle');
 var aside = document.getElementsByClassName('nav')[0];
 
 for(var i = 0; i < toggles.length; i++) {
 	toggles[i].addEventListener('click', function() {
-		aside.classList.toggle('toggled');
+		body.classList.toggle('nav-toggled');
 	});
 }
 
