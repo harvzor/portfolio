6afc74511a3f2f17f0db82c9b2c2620f2fc2fccd
Harvey Williams <harveyjwilliams@aol.co.uk>
2015-05-02 21:12:50 +0100

    Added swipe facility so that the side menu can be swiped in on a mobile.

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,17 @@
-var body = document.getElementsByTagName('body')[0];
-var toggles = document.getElementsByClassName('toggle');
-var aside = document.getElementsByClassName('nav')[0];
+(function() {
+	var body = document.getElementsByTagName('body')[0];
+	var toggles = document.getElementsByClassName('toggle');
+	var aside = document.getElementsByClassName('nav')[0];
 
-for(var i = 0; i < toggles.length; i++) {
-	toggles[i].addEventListener('click', function() {
-		body.classList.toggle('nav-toggled');
+	for(var i = 0; i < toggles.length; i++) {
+		toggles[i].addEventListener('click', function() {
+			body.classList.toggle('nav-toggled');
+		});
+	}
+
+	hljs.initHighlightingOnLoad();
+
+	new Menu(body, {
+		toggledClassName: 'nav-toggled'
 	});
-}
-
-hljs.initHighlightingOnLoad();
-
+})();
