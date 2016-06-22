3f4dbbc94f4e2793f93a4ba8abd2f0cb166f01dd
Harvey Williams <harveyjwilliams@aol.co.uk>
2016-06-22 01:10:52 +0100

    Added load out animation.

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@
 	var toggles = document.getElementsByClassName('toggle');
 	var aside = document.getElementsByClassName('nav')[0];
 	var footer = document.getElementById('footer');
+	var content = document.getElementById('content');
 
 	for (var i = 0; i < toggles.length; i++) {
 		toggles[i].addEventListener('click', function(event) {
@@ -17,6 +18,26 @@
 		new Menu(body, {
 			toggledClassName: 'nav-toggled'
 		});
+	}
+
+	var aElements = document.getElementsByTagName('a');
+
+	for (var i = 0; i < aElements.length; i++) {
+		aElements[i].addEventListener('click', function(e) {
+			var link = this;
+
+			e.preventDefault();
+
+			content.classList.add('exiting');
+
+			setTimeout(function() {
+				window.location.href = link.href;
+			}, 300);
+		});
+	}
+
+	window.onbeforeunload = function(event) { 
+		content.classList.add('exiting');
 	}
 })();
 
