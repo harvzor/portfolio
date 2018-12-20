3f290b91404f48a4f4dd9f751eff556aecb318f2
Harvey <harveyjwilliams@aol.co.uk>
2018-12-20 22:19:18 +0000

    Stopped Disqus from loading unless the user can see the comments.

--- src/js/script.js
+++ src/js/script.js
@@ -61,9 +61,53 @@ hw.nav = function() {
     }
 };
 
+// https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+hw.isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+/**
+ * Only load the Disqus comments if the user can see where the comments should be.
+*/
+hw.disqus = function() {
+    var disqusElement = document.getElementById('disqus_thread');
+    var loaded = false;
+
+    if (!disqusElement) {
+        return;
+    }
+
+    var run = function() {
+        var disqus_shortname = 'portfoliodisqus';
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    };
+
+    var scrollEvent = function() {
+        if (!loaded && hw.isInViewport(disqusElement)) {
+            loaded = true;
+
+            run();
+
+            window.removeEventListener('scroll', scrollEvent);
+        }
+    };
+
+    window.addEventListener('scroll', scrollEvent);
+};
+
 (function() {
     hw.nav();
     hljs.initHighlightingOnLoad();
     hw.youtubeVideoSetup();
     hw.exitingAnimation();
+    hw.disqus();
 })();
