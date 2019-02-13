66afcb0c5d078b4814bd360f8d5f3932604c3aeb
Harvey <harveyjwilliams@aol.co.uk>
2019-02-13 18:05:42 +0000

    Ensured that code highlighting script and CSS is only run if needed (also improved load speed).

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,48 @@
 var hw = {};
 
+hw.helpers = {
+    // https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
+    isInViewport: function(elem) {
+        var bounding = elem.getBoundingClientRect();
+
+        return (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    },
+    injectCssFileReference: function(href) {
+        var link = document.createElement('link');
+
+        link.href = href;
+        link.rel = 'stylesheet';
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+    },
+    injectJsFileReference: function(src, callback) {
+        var script = document.createElement('script');
+
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = src;
+
+        if (typeof callback !== 'undefined') {
+            // https://stackoverflow.com/questions/16839698/jquery-getscript-alternative-in-native-javascript
+            script.onload = script.onreadystatechange = function( _, isAbort ) {
+                if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+                    script.onload = script.onreadystatechange = null;
+                    script = undefined;
+
+                    if(!isAbort) { if(callback) callback(); }
+                }
+            };
+        }
+
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    }
+};
+
 hw.youtubeVideoSetup = function() {
     var testImage = function(src, callback) {
         var image = new Image();
@@ -74,7 +117,10 @@ hw.exitingAnimation = function() {
     }
 };
 
-hw.nav = function() {
+/**
+ * Setup navigation events.
+ */
+hw.navigationEvents = function() {
     var body = document.getElementsByTagName('body')[0];
     var toggles = document.getElementsByClassName('toggle');
 
@@ -89,22 +135,11 @@ hw.nav = function() {
     }
 };
 
-// https://gomakethings.com/how-to-test-if-an-element-is-in-the-viewport-with-vanilla-javascript/
-hw.isInViewport = function (elem) {
-    var bounding = elem.getBoundingClientRect();
-
-    return (
-        bounding.top >= 0 &&
-        bounding.left >= 0 &&
-        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-};
-
 /**
  * Only load the Disqus comments if the user can see where the comments should be.
 */
 hw.disqus = function() {
+    var disqus_shortname = 'portfoliodisqus';
     var disqusElement = document.getElementById('disqus_thread');
     var loaded = false;
 
@@ -112,18 +147,11 @@ hw.disqus = function() {
         return;
     }
 
-    var run = function() {
-        var disqus_shortname = 'portfoliodisqus';
-        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-    };
-
     var scrollEvent = function() {
-        if (!loaded && hw.isInViewport(disqusElement)) {
+        if (!loaded && hw.helpers.isInViewport(disqusElement)) {
             loaded = true;
 
-            run();
+            hw.helpers.injectJsFileReference('//' + disqus_shortname + '.disqus.com/embed.js');
 
             window.removeEventListener('scroll', scrollEvent);
         }
@@ -132,10 +160,21 @@ hw.disqus = function() {
     window.addEventListener('scroll', scrollEvent);
 };
 
+hw.codeHighlighting = function() {
+    if (document.querySelectorAll('pre code').length < 1) {
+        return;
+    }
+
+    hw.helpers.injectCssFileReference('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/styles/github.min.css');
+    hw.helpers.injectJsFileReference('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.5/highlight.min.js', function() {
+        hljs.initHighlightingOnLoad();
+    });
+};
+
 (function() {
-    hw.nav();
-    hljs.initHighlightingOnLoad();
+    hw.navigationEvents();
     hw.youtubeVideoSetup();
     hw.exitingAnimation();
+    hw.codeHighlighting();
     hw.disqus();
 })();
